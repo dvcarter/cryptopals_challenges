184c9f2231fc1b33eca61caf09bdcbae08ae614f
Desmond Carter <crtr@dsmnd.org>
2017-08-18 14:12:53 -0400

    Second cryptopals challenge solved

---
+++ fixed_XOR.rb
@@ -0,0 +1,19 @@
+# write a function that takes two equal-length buffers and produces their XOR combination
+#
+#
+#
+# test string: 1c0111001f010100061a024b53535009181c
+#
+# after hex decoding, and when XOR'd against: 686974207468652062756c6c277320657965
+#
+# the function should produce: 746865206b696420646f6e277420706c6179
+
+def fixXOR(s,t)
+    u = s.hex ^ t.hex
+    u.to_s(16)
+end
+
+str0 = "1c0111001f010100061a024b53535009181c"
+str1 = "686974207468652062756c6c277320657965"
+
+p fixXOR(str0, str1)
