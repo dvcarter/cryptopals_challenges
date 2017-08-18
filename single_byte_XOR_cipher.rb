0bdce926ec23c8181e3e24d0cd731e191f9878d3
Desmond Carter <crtr@dsmnd.org>
2017-08-18 14:20:00 -0400

    Start third cryptopals challenge

---
+++ single_byte_XOR_cipher.rb
@@ -0,0 +1,9 @@
+#
+# hex encoded string:  "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+#
+# has been XOR'd against a single character.
+#
+# Find the key, decrypt the message. Do it by hand. Don't write code to do it for you.
+#
+# Devise some method for "Scoring" a piece of english plaintext. Character frequency is a good metric.
+# Evaluate each output and choose the one with the best score.
